ad956383cb1b526c17b53d90269c70c1f5d88c94
Harshit <harshitkholiya1003@gmail.com>
2026-08-15 10:34:20 +0530

    Foregin key

--- dj1/DRF1/app1/models.py
+++ dj1/DRF1/app1/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.CASCADE,null=True, blank=True, related_name='people') # person_set.all() -> perple.all() 
     
     def __str__(self):
         return self.name
--- dj1/DRF1/app1/serializers.py
+++ dj1/DRF1/app1/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person, Color
+
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = ['name']
+
+
+
 class PeopleSerializer(serializers.ModelSerializer):
 
+    color= ColorSerializer()
     class Meta :
         model=Person
         fields='__all__'
+        # depth=1
